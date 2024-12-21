df7584bfffe270f0d33789766a415edac353a233
Lisa Rose Jepson <LisaRoseJepson@users.noreply.github.com>
2024-12-21 17:40:41 +0000

    Flashing tree lights

--- XmasTree.py
+++ XmasTree.py
@@ -1,27 +1,35 @@
 from tree import RGBXmasTree
 from time import sleep
 from colorzero import Color
+import random
 
 tree = RGBXmasTree()
-i = 0
+#i = 0
 #tree.color = Color('red')
 
 #for pixel in tree:
 #	pixel.color = Color('purple')
 #	sleep(1)
 
-for pixel in tree:
-	
-	if i == 3:
-		tree[i].color = Color('yellow')
-	elif i in [0, 6, 7, 12, 15, 16, 19, 24]:
-		tree[i].color = Color('blue')
-	elif i in [1, 5, 8, 11, 14, 17, 20, 23]:
-		tree[i].color = Color('red')
-	else:
-		tree[i].color = Color('green')
-	i += 1
+def lights():
+	i = 0
+	for pixel in tree:
 
+		if i == 3:
+			tree[i].color = Color('yellow')
+		elif random.randint == 1:
+ 			if i in [0, 6, 7, 12, 15, 16, 19, 24]:
+				tree[i].color = (0, 0, 1)
+			elif i in [1, 5, 8, 11, 14, 17, 20, 23]:
+				tree[i].color = (1, 0, 0)
+			else:
+				tree[i].color = (0, 0, 1) 
+		else:
+			tree[i].color = (0, 0, 0)
+		i += 1
+	time.sleep(1)
 
+while True:
+	lights()
 #for pixel in tree:
 #	print(pixel)
